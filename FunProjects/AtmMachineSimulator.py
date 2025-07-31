f471c8ff4e902437234bee40625416ff39deeaaa
Santhosh09062006 <sandy09062006@gmail.com>
2025-07-31 09:34:29 +0530

    Fix: deposit method condition to check for positive amount

--- FunProjects/AtmMachineSimulator.py
+++ FunProjects/AtmMachineSimulator.py
@@ -4,7 +4,7 @@ class ATM:
         self.balance = balance
 
     def deposit(self, amount):
-        if amount >= 0:
+        if amount <= 0:
             print("Amount must be positive.")
         else:
             self.balance += amount
